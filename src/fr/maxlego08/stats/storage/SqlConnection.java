177315b4903b1d716db70cd84e7138c12569b71d
Maxlego08 <maxlego08@sfr.fr>
2024-05-04 12:04:15 +0200

    :memo: Add SQLITE support

--- src/fr/maxlego08/stats/storage/SqlConnection.java
+++ src/fr/maxlego08/stats/storage/SqlConnection.java
@@ -3,6 +3,8 @@ package fr.maxlego08.stats.storage;
 import fr.maxlego08.sarah.DatabaseConfiguration;
 import fr.maxlego08.sarah.DatabaseConnection;
 import fr.maxlego08.sarah.MySqlConnection;
+import fr.maxlego08.sarah.SqliteConnection;
+import fr.maxlego08.sarah.database.DatabaseType;
 import fr.maxlego08.stats.StatsPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 
@@ -15,13 +17,14 @@ public class SqlConnection {
     public SqlConnection(StatsPlugin plugin) {
         FileConfiguration configuration = plugin.getConfig();
         boolean debug = configuration.getBoolean("sql.debug", false);
+        DatabaseType databaseType = DatabaseType.valueOf(configuration.getString("sql.type", DatabaseType.MYSQL.name()).toString());
         String host = configuration.getString("sql.host");
         int port = configuration.getInt("sql.port");
         String database = configuration.getString("sql.database");
         String user = configuration.getString("sql.user");
         String password = configuration.getString("sql.password");
         DatabaseConfiguration databaseConfiguration = DatabaseConfiguration.create(user, password, port, host, database, debug);
-        this.databaseConnection = new MySqlConnection(databaseConfiguration);
+        this.databaseConnection = databaseType == DatabaseType.MYSQL ? new MySqlConnection(databaseConfiguration) : new SqliteConnection(databaseConfiguration, plugin.getDataFolder());
     }
 
     public Connection getConnection() {
